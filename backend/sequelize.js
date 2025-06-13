0f95c2da54997e5fb854f196fcd0d81065229596
Danil <dedov7347@gmail.com>
2025-06-13 13:44:38 +0000

    frontend finally works, backend too, now testing deploy and pulling

--- backend/sequelize.js
+++ backend/sequelize.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
+const sequelize = new Sequelize(process.env.DATABASE_URL??"sqlite:./database.sqlite", {
   dialect: 'sqlite', // or "postgres" / "mysql"
   storage: './database.sqlite', // for SQLite
   logging: console.log,
