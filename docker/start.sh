fa80db6c83b3aeb01256448dd8bd9c270a534451
Danil <dedov7347@gmail.com>
2025-06-13 12:02:28 +0000

    fixed frontend build bug

--- docker/start.sh
+++ docker/start.sh
@@ -9,7 +9,8 @@ PORT=$APP_PORT node /app/backend/server.js &
 # Check if FRONTEND env var is set to true
 if [ "$FRONTEND" = "true" ]; then
   echo "Starting frontend on port 3000..."
-  serve -s /app/frontend/out -l 3000
+  cd /app/frontend
+  npm run start
 else
   echo "Frontend not included. Only backend is running."
   wait
